60e477f04e551332d43586105b5f49aff587fadd
Hasin Hayder <countdraculla@gmail.com>
2023-11-24 17:19:57 +0600

    FAQs

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -9,8 +9,19 @@ use Illuminate\Support\Facades\Redirect;
 
 class FaqController extends Controller
 {
-    public function groupIndex(): View
+    public function groupIndex(Request $request): View
     {
+        //check if it is a POST request
+        if ($request->isMethod('post')) {
+            $group = new Group();
+
+            $group->name = $request->name;
+            $group->description = $request->description;
+            $group->shop_id = auth()->user()->id;
+            $group->status = 1;
+
+            $group->save();
+        }
         $groups = Group::where('shop_id', auth()->user()->id)->get();
         return view('group.index', compact('groups'));
     }
@@ -26,7 +37,6 @@ class FaqController extends Controller
         $group->status = 1;
 
         $group->save();
-
         return Redirect::tokenRedirect('group.index');
 
     }
--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -6,7 +6,7 @@
     <section class="bg-gray-100 hidden" id="create-group">
         <div class="py-14">
             <div class="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
-                <div class="gap-12">
+                <div class="gap-12 flex justify-between">
                     <div class="max-w-lg space-y-3">
                         <h3 class="text-indigo-600 font-semibold">Groups</h3>
                         <p class="text-gray-800 text-3xl font-semibold sm:text-4xl">
@@ -17,6 +17,11 @@
                             faq
                             to them.
                         </p>
+                    </div>
+                    <div>
+                        <button onclick="hideCreateGroup()" class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+                            Cancel
+                        </button>
                     </div>
                 </div>
 
@@ -115,6 +120,10 @@
 
         function showCreateGroup() {
             document.getElementById('create-group').classList.remove('hidden');
+        }
+
+        function hideCreateGroup(){
+            document.getElementById('create-group').classList.add('hidden');
         }
         // function saveGroup() {
         //     axios.post('/groups', {
