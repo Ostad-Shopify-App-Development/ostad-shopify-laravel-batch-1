6bdfba9cac3896192d29a0ccfd1c1b50d870050f
Hasin Hayder <countdraculla@gmail.com>
2023-12-14 21:55:51 +0600

    M7C2

--- public/js/ostad.js
+++ public/js/ostad.js
@@ -1,18 +1,18 @@
 // console.log(Shopify);
-const createOstIframe = () => {
-    const ostGeneralFaq = document.getElementById('ostad-general-faq');
-    const ostLink = `https://happy-epic-dog.ngrok-free.app/storefront/widgets/general-faq?shop=${Shopify.shop}`;
-    const ostIframe = document.createElement('iframe');
+// const createOstIframe = () => {
+//     const ostGeneralFaq = document.getElementById('ostad-general-faq');
+//     const ostLink = `https://happy-epic-dog.ngrok-free.app/storefront/widgets/general-faq?shop=${Shopify.shop}`;
+//     const ostIframe = document.createElement('iframe');
 
-    ostIframe.src = ostLink;
-    ostIframe.frameBorder = "0";
-    ostIframe.scrolling = "yes";
-    ostIframe.width = "100%";
-    ostIframe.height = "100%";
-    ostIframe.style = "min-height: 420px;";
+//     ostIframe.src = ostLink;
+//     ostIframe.frameBorder = "0";
+//     ostIframe.scrolling = "yes";
+//     ostIframe.width = "100%";
+//     ostIframe.height = "100%";
+//     ostIframe.style = "min-height: 420px;";
 
-    ostGeneralFaq.appendChild(ostIframe);
-};
+//     ostGeneralFaq.appendChild(ostIframe);
+// };
 
 // createOstIframe();
 
